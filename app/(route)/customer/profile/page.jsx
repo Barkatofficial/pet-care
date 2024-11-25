6615631d1eb603ca71e17be79dff88ef9ac49cbf
Sayan Mukherjee <108444636+Sayan044@users.noreply.github.com>
2024-11-25 00:10:57 +0000

    profile page

---
+++ app/(route)/customer/profile/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Profile from './_components/Profile'
+
+
+export default function Page() {
+
+  return (
+    // <div>page</div>
+    // PASS THE FETCHED PROFILE DATA INTO COMPONENT USING PROPS
+    <Profile />
+  )
+}
