c80f8d42ed642d75e3106398a1528beb1f5bce95
sayan044 <sayan.mukherjee.work044@gmail.com>
2024-12-28 18:13:45 +0530

    .

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,25 @@
+"use client"
 import Hero from "./_components/Hero";
 import CategoryListContainer from "./_components/CategoryListContainer";
 import Testimonials  from "./_components/Testimonials";
 import Summarization from "./_components/Summarization";
 import FAQ from "./_components/FAQ";
 import StatsSection from "./_components/StatsSection";
+import { useSearchParams } from 'next/navigation';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Page() {
+  const searchParams = useSearchParams();
+  const email = searchParams.get('email');
+  const phone = "+91 6294096170"
+  
   return (
+    
+    
     <>
+
+    
+    
       <Hero />
       <CategoryListContainer />
       {/* <StatsSection /> */}
